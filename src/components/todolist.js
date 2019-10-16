83fe33a3e51685b42d2c4ef10b9f5cbc28cbf694
Dragod <sirawit.aarm@gmail.com>
2019-10-16 15:31:40 +0700

    😍 add todo list hardcode no interactive

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,6 +1,20 @@
 import React from "react";
+import Todo from "./todo";
 
 export default function TodoList() {
+    const todos = [
+        {
+            id: Date.now(),
+            value: "Buy milk",
+            done: false
+        },
+        {
+            id: Date.now() + 1,
+            value: "Play with dog",
+            done: true
+        }
+    ];
+
     return (
         <div>
             <div className="hero is-info">
@@ -8,10 +22,12 @@ export default function TodoList() {
                     <p className="title is-1">Todos</p>
                 </div>
             </div>
-            
+
             <section className="section">
                 <div className="container">
-                    Todos will show up here later
+                    {todos.map(todo => (
+                        <Todo key={todo.id} todo={todo}/>
+                    ))}
                 </div>
             </section>
         </div>
