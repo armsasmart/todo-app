7133d27302770855fb64a4016cf68d9af2b81c11
Dragod <sirawit.aarm@gmail.com>
2019-10-16 18:49:26 +0700

    😍 use context api for todo list

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,23 @@
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
+import {TodoContext} from "../TodoContext";
 
 export default function Navbar() {
+    const [todos, setTodos] = useContext(TodoContext);
     const [menu, setMenu] = useState(false);
+    const [value, setValue] = useState("");
+
+    const addTodo = (e) => {
+        e.preventDefault();
+        setTodos([
+            ...todos,
+            {
+                id: Date.now(),
+                value: value,
+                done: false
+            }
+        ]);
+        setValue("");
+    };
 
     return (
         <div className="navbar has-shadow">
@@ -23,19 +39,20 @@ export default function Navbar() {
             <div className={`navbar-menu ${menu ? "is-active" : ""}`}>
                 <div className="navbar-end">
                     <div className="navbar-item">
-                        <div className="field has-addons">
+                        <form onSubmit={addTodo} className="field has-addons">
                             <p className="control is-expanded">
-                                <input type="text" className="input"/>
+                                <input type="text" className="input"
+                                       value={value} onChange={(e) => setValue(e.target.value)}/>
                             </p>
                             <p className="control">
                                 <button className="button is-info has-text-weight-bold">
                                     Add Todo
                                 </button>
                             </p>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 };
